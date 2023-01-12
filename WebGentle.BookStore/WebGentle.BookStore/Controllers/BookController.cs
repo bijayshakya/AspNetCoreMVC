5b80e033bb6d1b311a12b84e3291d5ff6c21aa27
i23501 <bshakya@verisk.com>
2023-01-12 22:46:06 +0545

    Implemented MVC model

---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGentle.BookStore.Models;
+using WebGentle.BookStore.Repository;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+           _bookRepository = new BookRepository(); 
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+
+            return View();
+        }
+
+        public ViewResult GetBook(int id)
+        {
+            //return _bookRepository.GetBookById(id);
+            return View();
+        }
+
+       
+       public ViewResult SearchBook(string bookName, string authorName)
+        {
+            //return _bookRepository.SearchBook(bookName,authorName);
+            return View();
+        }
+    }
+}
--- WebGentle.BookStore/WebGentle.BookStore/Startup.cs
+++ WebGentle.BookStore/WebGentle.BookStore/Startup.cs
@@ -18,6 +18,7 @@ namespace WebGentle.BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
